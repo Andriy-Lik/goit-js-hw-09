cc47b386b8f30bc50b0f094edba5a364349e83e8
Andriy-Lik <alan321987@gmail.com>
2022-11-22 22:05:29 +0000

    Deploying to gh-pages from @ Andriy-Lik/goit-js-hw-09@e0242f7c5c5d3328ae211508330321961840af8a 🚀

---
+++ 01-color-switcher.b10debba.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]"),bodyBgc:document.querySelector("body")};let e=null;t.btnStart.addEventListener("click",(function(){e=setInterval((()=>{t.bodyBgc.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.btnStart.disabled=!0,t.btnStop.disabled=!1})),t.btnStop.addEventListener("click",(function(){clearInterval(e),t.btnStart.disabled=!1,t.btnStop.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.b10debba.js.map
